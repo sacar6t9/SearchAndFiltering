825bc4565c22b74ac73bb914b428ead924041f59
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-02 10:49:45 +0545

    finshed price and category section

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -1,10 +1,28 @@
-import './Category.css'
+import "./Category.css";
 const Category = () => {
   return (
     <div>
-    <h1> category</h1>  
+      <h2 className="sidebar-title">Category</h2>
+    <div>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>All
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Flats
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Sandal
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Heels
+      </label>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Category
+export default Category;
--- src/Sidebar/Price/Price.jsx
+++ src/Sidebar/Price/Price.jsx
@@ -1,9 +1,29 @@
 import './Price.css'
 
 const Price = () => {
-  return (
-    <div>
-      <h1>price</h1>
+  return ( 
+    <div className='ml'>
+    <h1 className='sidebar-title price-title'> Price</h1>
+    <label  className="sidebar-title-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"> </span>All
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"> </span>$0-$50
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"> </span>$50-$100
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"> </span>$100-$150
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test2" />
+        <span className="checkmark"> </span>over $150
+      </label>
     </div>
   )
 }
