df0e14e948be09b7d5a19cf6a938fa0f21abb959
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-02 11:07:40 +0545

    finished color section

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -3,9 +3,30 @@ import './Colors.css'
 const Colors = () => {
   return (
     <div>
-      <h1> colors
-        
-      </h1>
+       <label  className="sidebar-title-container color-title">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>All
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Black
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />  
+        <span className="checkmark"> </span>Blue
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Red
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>Green
+      </label>
+      <label  className="sidebar-title-container">
+        <input type="radio" name="test" />
+        <span className="checkmark"> </span>White
+      </label>
     </div>
   )
 }
