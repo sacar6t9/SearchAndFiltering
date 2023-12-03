5d163d2f83836590fc04966522fa00b049a9ddc6
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-03 19:26:11 +0545

    added input functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import Nav from './Navigation/Nav'
-import Products from './Products/Products'
-import Recommendend from './Recommendend/Recommendend'
-import Sidebar from './Sidebar/Sidebar'
+import React, { useState } from "react";
+import Nav from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommendend from "./Recommendend/Recommendend";
+import Sidebar from "./Sidebar/Sidebar";
+//DATABASE
+import products from "./db/data";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  //input
+  const [query, setQuery] = useState("");
+  const handleInputChange = (event) => {
+    setQuery(event.target.value);
+  };
+  const filteredItems = products.filter((product) =>
+    product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
+  
   return (
     <>
-      <Sidebar/>
-      <Nav/>
-      <Recommendend/>
-      <Products/>
+      <Sidebar />
+      <Nav />
+      <Recommendend />
+      <Products />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
