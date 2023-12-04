f483df1281055cd14c7130c0810893faae7b6114
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-05 00:38:59 +0545

    update some code

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -6,7 +6,7 @@ const Colors = ({ handleChange }) => {
     <div>
       <h2 className="sidebar-title color-title">Colors</h2>
       <label className="sidebar-label-container">
-        <input type="radio" value="" name="test1" onChange={handleChange} />
+        <input type="radio" value="" name="test3" onChange={handleChange} />
         <span className="checkmark all"></span>All
       </label>
       <label className="sidebar-label-container">
@@ -16,32 +16,32 @@ const Colors = ({ handleChange }) => {
         handleChange={handleChange}
         value="black"
         title="Black"
-        name="test1"
+        name="test3"
         color="black"
       />
       <Input
         handleChange={handleChange}
         value="blue"
         title="Blue"
-        name="test1"
+        name="test3"
         color="blue"
       />
       <Input
         handleChange={handleChange}
         value="red"
         title="Red"
-        name="test1"
+        name="test3"
         color="red"
       />
       <Input
         handleChange={handleChange}
         value="green"
         title="Green"
-        name="test1"
+        name="test3"
         color="green"
       />
       <label className="sidebar-label-container">
-        <input type="radio" value="" name="test1" onChange={handleChange} />
+        <input type="radio" value="" name="test3" onChange={handleChange} />
         <span
           className="checkmark"
           style={{ background: "white", border: "2px solid black" }}
