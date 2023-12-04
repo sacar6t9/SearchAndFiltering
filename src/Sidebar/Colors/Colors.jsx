143f308c54f84df6883b39539e1694381d5d18d3
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-04 20:17:33 +0545

    color components refactor

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,16 +1,55 @@
-import Input from '../../components/Input'
-import './Colors.css'
+import Input from "../../components/Input";
+import "./Colors.css";
 
-const Colors = () => {
+const Colors = ({ handleChange }) => {
   return (
     <div>
-    <h2 className="sidebar-title color-title">Colors</h2>
-       <Input/>
-       <Input/>
-       <Input/>
-       <Input/>
+      <h2 className="sidebar-title color-title">Colors</h2>
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test1" onChange={handleChange} />
+        <span className="checkmark all"></span>All
+      </label>
+      <label className="sidebar-label-container">
+        <input type="radio" name="test2" value="" onChange={handleChange} />
+      </label>
+      <Input
+        handleChange={handleChange}
+        value="black"
+        title="Black"
+        name="test1"
+        color="black"
+      />
+      <Input
+        handleChange={handleChange}
+        value="blue"
+        title="Blue"
+        name="test1"
+        color="blue"
+      />
+      <Input
+        handleChange={handleChange}
+        value="red"
+        title="Red"
+        name="test1"
+        color="red"
+      />
+      <Input
+        handleChange={handleChange}
+        value="green"
+        title="Green"
+        name="test1"
+        color="green"
+      />
+      <label className="sidebar-label-container">
+        <input type="radio" value="" name="test1" onChange={handleChange} />
+        <span
+          className="checkmark"
+          style={{ background: "white", border: "2px solid black" }}
+        ></span>{" "}
+        white
+      </label>
     </div>
-  )
-}
+  );
+};
 
-export default Colors
+export default Colors;
