c222683e97365ffe0947052a765d6faaf0856ddd
Bishal Bhattarai <bishalbhattarai921@gmail.com>
2023-12-04 09:28:20 +0545

    catefory component refactor

--- src/Sidebar/Category/Category.jsx
+++ src/Sidebar/Category/Category.jsx
@@ -1,16 +1,40 @@
 import Input from "../../components/Input";
 import "./Category.css";
-const Category = () => {
+const Category = ({ handleChange }) => {
   return (
     <div>
       <h2 className="sidebar-title">Category</h2>
-    <div>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-     
-    </div>
+      <div>
+        <label className="sidebar-label-container">
+          <input onChange={handleChange} type="radio" value="" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <Input 
+        handleChange={handleChange}
+        value="sneakers"
+        title="Sneakers"
+        name='test'
+        />
+         
+         <Input 
+        handleChange={handleChange}
+        value="falts"
+        title="Falts"
+        name='test'
+        />
+         <Input 
+        handleChange={handleChange}
+        value="sandals"
+        title="Sandals"
+        name='test'
+        />
+         <Input 
+        handleChange={handleChange}
+        value="heels"
+        title="Heels"
+        name='test'
+        />
+      </div>
     </div>
   );
 };
